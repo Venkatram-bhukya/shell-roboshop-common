45495d827b20951c6af3eb3af80201715f8c21a1
bhukyadevops@gmail.com <bhukyadevops@gmail.com>
2026-07-15 00:29:03 +0530

    shell-roboshop-common

--- mongodb.sh
+++ mongodb.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
 
-source ./common.sh
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+    exit 1
+fi
 
-check_root
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
+    fi
+}
 
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 VALIDATE $? "Copying Mongo Repo" 
@@ -21,6 +40,4 @@ sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 VALIDATE $? "Allowing remote connections"
 
 systemctl restart mongod
-VALIDATE $? "Restarted MongoDB"
-
-print_total_time
+VALIDATE $? "Restarted MongoDB" 
